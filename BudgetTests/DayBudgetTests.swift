fa88e31ee76be0f0c48e58eec47c22ae9e143f15
marijnschilling <marijnschilling@gmail.com>
2018-08-22 17:31:00 +0200

    [WIP]

---
+++ BudgetTests/DayBudgetTests.swift
@@ -0,0 +1,34 @@
+//
+//  BudgetTests.swift
+//  BudgetTests
+//
+//  Created by Marijn Schilling on 22/08/2018.
+//  Copyright © 2018 Bakken & Bæck. All rights reserved.
+//
+
+import XCTest
+@testable import Budget
+
+class DayBudgetTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+    }
+
+    func testDayBudgetInitialization() {
+        let dayBudget = DayBudget(budget: 20, date: <#T##Date#>, expenses: <#T##[Int]?#>)
+    }
+
+    func testAddExpense() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testBalance() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
