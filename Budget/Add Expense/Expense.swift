2b03a02874382d35a4a3fd6f86ae222ecdd6bf0f
marijnschilling <marijnschilling@gmail.com>
2018-10-15 10:00:03 +0200

    Add expense struct

---
+++ Budget/Add Expense/Expense.swift
@@ -0,0 +1,21 @@
+//
+// Created by Marijn Schilling on 15/10/2018.
+// Copyright (c) 2018 Bakken & Bæck. All rights reserved.
+//
+
+import Foundation
+
+enum Currency {
+    case euro
+    case dollar
+}
+
+struct Expense {
+    let amount: Int
+    let currency: Currency
+
+    init(amount: Int, currency: Currency = .euro) {
+        self.amount = amount
+        self.currency = currency
+    }
+}
